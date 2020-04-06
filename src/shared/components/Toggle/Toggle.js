f53b5aa50a6c9397469e4bdeb5c9ba268982b7e7
mile_ignjatovic <mile.ignjatovic@endava.com>
2020-04-06 16:18:30 +0200

    Toggle rework, secionTitle component created

--- src/shared/components/Toggle/Toggle.js
+++ src/shared/components/Toggle/Toggle.js
@@ -9,12 +9,33 @@ const Toggle = (props) => {
         setToggleState(!toggleState);
         props.toggle(toggleState);
     }
+
+    let toggleOptions = props.toggleOptions ? props.toggleOptions : ['option one', 'option two'];
     
-    let labelClasses = toggleState ? [classes['Toggle-span'], classes['Toggle-span-false']].join(' ') : classes['Toggle-span'];
+    let labelClassesOne = !toggleState ? [classes['Toggle-span'], classes['Toggle-span-false']].join(' ') : classes['Toggle-span'];
+    let labelClassesTwo = toggleState ? [classes['Toggle-span'], classes['Toggle-span-false']].join(' ') : classes['Toggle-span'];
+    
+    let toggleOptionOne = toggleOptions[0] ? ( <span className={labelClassesOne}>
+        {toggleOptions[0]}
+    </span>): null; 
+    let toggleOptionTwo = toggleOptions[1] ? ( <span className={labelClassesTwo}>
+        {toggleOptions[1]}
+    </span>): null; 
+
     return (
-        <div className={classes.Toggle}>
-            <input  className={classes['Toggle-input']} type="checkbox" id="switch" onChange={() => {changeToggle()}}/><label className={classes['Toggle-label']} htmlFor="switch"></label>
-            <span className={labelClasses}>{props.children}</span>
+        <div 
+            className={classes.Toggle}>
+           {toggleOptionOne}
+            <input  
+                className={classes['Toggle-input']} 
+                type="checkbox" 
+                id="switch" 
+                onChange={() => {changeToggle()}}/>
+            <label 
+                className={classes['Toggle-label']} 
+                htmlFor="switch">
+            </label>
+            {toggleOptionTwo}
         </div>
     );
 }
