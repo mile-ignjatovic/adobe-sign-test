11339488fbe97b7f348cfb248872e34ec021651f
mile_ignjatovic <mile.ignjatovic@endava.com>
2020-04-06 17:43:48 +0200

    app js clean

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.module.css';
-import MobxTest from './components/MobxTest/MobxTest';
-import MobxTestStoreProvider from './components/MobxTest/MobxTestStore';
-import StyledComponentTest from './components/StyledComponentTest/StyledComponentTest';
-import Test from './components/Test/Test';
+import Send from './components/Send/Send';
 
 function App() {
+
   return (
     <BrowserRouter>
       <main className="App">
-        App works!
-        <hr />
-        Routing test:
-        <br />
-        <Link to="mobx">Test Mobx</Link>
-        <br />
-        <Link to="styled">Test StyledComponent</Link>
-
-        <Route path="/" exact component={Test} />
-        <MobxTestStoreProvider>
-          <Route path="/mobx" exact component={MobxTest} />
-        </MobxTestStoreProvider>
-        <Route path="/styled" exact component={StyledComponentTest} />
+       <Send></Send>
       </main>
     </BrowserRouter>
   );
