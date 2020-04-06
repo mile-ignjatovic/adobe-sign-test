85b843ca9f2975836d799cb649aa6319d02f13bb
mile_ignjatovic <mile.ignjatovic@endava.com>
2020-04-06 14:48:46 +0200

    created send component

---
+++ src/components/Send/Send.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Send.module.css';
+
+const Send = (props) => {
+    return (
+        <section>
+            
+        </section>
+    );
+}
+
+export default Send;
